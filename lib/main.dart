54571d52891c518f47c592e12ec6d6572de1a08c
josimarminetefera <josimaminete@gmail.com>
2020-07-01 23:14:54 -0300

    Criando tela principal do app

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:fluttertube/api.dart';
 import 'package:fluttertube/screens/home_screen.dart';
 
-void main(){
-
-  Api api = Api();
-  api.buscar("eletro");
-
+void main() {
+  //Api api = Api();
+  //api.buscar("eletro");
   runApp(MyApp());
 }
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,52 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("images/youtube-logo-1.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: <Widget>[
+          IconButton(
+            icon: Stack(
+              children: <Widget>[
+                Icon(Icons.star),
+                Positioned(
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.all(1),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    constraints: BoxConstraints(
+                      minWidth: 12,
+                      minHeight: 12,
+                    ),
+                    child: new Text(
+                      '10',
+                      style: new TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
